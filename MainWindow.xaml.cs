6a4f2644f2de2b8f702172280c9b8ab9a871cb5d
mbul76 <bullinger-marvin@web.de>
2024-01-26 16:21:52 +0100

    datenbankanbindung komplett

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,6 @@ using System.Windows.Shapes;
 namespace MMS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
