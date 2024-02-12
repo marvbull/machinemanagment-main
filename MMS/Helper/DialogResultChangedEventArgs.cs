de9179fe3736cb5ad8f8ca6c55ab5f21d28110e9
Carla Vogel <carla.vogel@unitybox.de>
2024-02-12 08:54:38 +0100

    ID Gelungen

---
+++ MMS/Helper/DialogResultChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMS.Helper
+{
+    public class DialogResultChangedEventArgs : EventArgs
+    {
+        public bool DialogResult { get; }
+
+        public DialogResultChangedEventArgs(bool dialogResult)
+        {
+            DialogResult = dialogResult;
+        }
+    }
+}
--- MMS/ViewModel/MainMMSViewModel.cs
+++ MMS/ViewModel/MainMMSViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using FontAwesome.Sharp;
+using MMS.View;
 using MMSLib.Model;
-
 
 namespace MMS.ViewModel
 {
@@ -63,9 +64,81 @@ namespace MMS.ViewModel
 
         private void ExecuteAuftragAnlegenViewCommand(object? obj)
         {
-            CurrentChildView = new AuftragAnlegenViewModel();
-            Caption = "Auftrag anlegen";
-            Icon = IconChar.Coffee;
+            var vorgesetzterEingabeViewModel = new VorgesetzterIDEingabeViewModel();
+            var vorgesetzterEingabeView = new VorgesetzterIDEingabeView();
+            vorgesetzterEingabeView.DataContext = vorgesetzterEingabeViewModel;
+
+            vorgesetzterEingabeViewModel.DialogResultChanged += (sender, e) =>
+            {
+                if (e.DialogResult)
+                {
+                    CurrentChildView = new AuftragAnlegenViewModel();
+                    Caption = "Auftrag anlegen";
+                    Icon = IconChar.Coffee;
+                }
+            };
+
+            vorgesetzterEingabeView.ShowDialog();
         }
     }
 }
+        //private ViewModelBase? _currentChildView; // Nullable, da beim Start nicht unbedingt initialisiert
+        //private string? _caption; // Nullable, da es keinen initialen Wert hat
+        //private IconChar _icon; // Non-nullable, da IconChar ein Enum ist
+
+        //public ViewModelBase? CurrentChildView
+        //{
+        //    get { return _currentChildView; }
+        //    set
+        //    {
+        //        _currentChildView = value;
+        //        OnPropertyChanged(nameof(CurrentChildView));
+        //    }
+        //}
+
+        //public string? Caption
+        //{
+        //    get { return _caption; }
+        //    set
+        //    {
+        //        _caption = value;
+        //        OnPropertyChanged(nameof(Caption));
+        //    }
+        //}
+
+        //public IconChar Icon
+        //{
+        //    get { return _icon; }
+        //    set
+        //    {
+        //        _icon = value;
+        //        OnPropertyChanged(nameof(Icon));
+        //    }
+        //}
+
+        //public ICommand ShowCheckViewCommand { get; }
+        //public ICommand ShowAuftragAnlegenViewCommand { get; }
+
+        //public MainMMSViewModel()
+        //{
+        //    ShowCheckViewCommand = new ViewModelCommand(ExecuteShowCheckViewCommand);
+        //    ShowAuftragAnlegenViewCommand = new ViewModelCommand(ExecuteAuftragAnlegenViewCommand);
+
+        //    // Standard-View beim Start
+        //    ExecuteShowCheckViewCommand(null);
+        //}
+
+        //private void ExecuteShowCheckViewCommand(object? obj)
+        //{
+        //    CurrentChildView = new CheckViewModel();
+        //    Caption = "Check";
+        //    Icon = IconChar.Computer;
+        //}
+
+        //private void ExecuteAuftragAnlegenViewCommand(object? obj)
+        //{
+        //    CurrentChildView = new AuftragAnlegenViewModel();
+        //    Caption = "Auftrag anlegen";
+        //    Icon = IconChar.Coffee;
+        //}
+
